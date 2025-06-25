282cfd0b213853e6b98f5bead4471c6049d9f57b
Charlie <charlie@mptree.com>
2025-06-25 14:51:11 +0800

    mapletree: add prettier config

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -2,20 +2,20 @@ import { ElectronAPI } from '@electron-toolkit/preload';
 
 declare global {
   interface Window {
-    electron: ElectronAPI
+    electron: ElectronAPI;
     api: {
-      setIgnoreMouseEvents: (ignore: boolean) => void
-      toggleForceIgnoreMouse: () => void
-      onForceIgnoreMouseChanged: (callback: (isForced: boolean) => void) => void
-      onModeChanged: (callback: (mode: 'pet' | 'window') => void) => void
-      showContextMenu: (x: number, y: number) => void
-      onMicToggle: (callback: () => void) => void
-      onInterrupt: (callback: () => void) => void
-      updateComponentHover: (componentId: string, isHovering: boolean) => void
-      onToggleInputSubtitle: (callback: () => void) => void
-      onToggleScrollToResize: (callback: () => void) => void
-      onSwitchCharacter: (callback: (filename: string) => void) => void
-    }
+      setIgnoreMouseEvents: (ignore: boolean) => void;
+      toggleForceIgnoreMouse: () => void;
+      onForceIgnoreMouseChanged: (callback: (isForced: boolean) => void) => void;
+      onModeChanged: (callback: (mode: 'pet' | 'window') => void) => void;
+      showContextMenu: (x: number, y: number) => void;
+      onMicToggle: (callback: () => void) => void;
+      onInterrupt: (callback: () => void) => void;
+      updateComponentHover: (componentId: string, isHovering: boolean) => void;
+      onToggleInputSubtitle: (callback: () => void) => void;
+      onToggleScrollToResize: (callback: () => void) => void;
+      onSwitchCharacter: (callback: (filename: string) => void) => void;
+    };
   }
 }
 
